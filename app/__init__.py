e37e1437e980e1ccb1968af4541ea14ec446a142
Mike Horn <mike@mikehorn.net>
2025-09-18 17:00:58 +0100

    Refactor database connection handling to use environment variables and improve data migration script for Azure compatibility

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate  # Tech Debt: my be able to remove
 import os, secrets
 import dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 # Initialise extensions globally
 db = SQLAlchemy()
@@ -21,7 +23,7 @@ def create_app():
     app.secret_key = secrets.token_hex(32)
     basedir = os.path.abspath(os.path.dirname(__file__))
     db_path = os.path.join(basedir, '..', 'service_standards.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", f"sqlite:///{db_path}")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     migrate.init_app(app, db)
@@ -32,3 +34,15 @@ def create_app():
     return app
     
 
+def build_engine():
+    conn_str = os.environ["DATABASE_URL"]
+    engine = create_engine(
+        conn_str,
+        pool_pre_ping=True,       # drops stale connections
+        pool_recycle=1800,        # recycle every 30 mins
+        fast_executemany=True     # faster bulk inserts with pyodbc
+    )
+    return engine
+
+
+    
